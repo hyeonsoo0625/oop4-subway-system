894baeed60dc92707fbce0ae3281b8a51db245dc
Hyeonsoo0625 <hyeonsoo02@gmail.com>
2023-11-29 12:49:43 +0900

    Feat: 다익스트라 정수형 문제 해결

--- subway/main.cpp
+++ subway/main.cpp
@@ -28,36 +28,28 @@ int main() {
     cin >> start;
     cout << "도착지 : ";
     cin >> end;
-    unordered_map<string, int> d;
+    unordered_map<string, double> d;
     unordered_map<string, Station> station; //역 이름, 역 객체
-
-    // 역 정보 정리
-    ifstream stationInfo("subway_information.txt");
-    string stationl;
-    while (getline(stationInfo, stationl)) {// id:역이름
-        vector<string> v = split(stationl, ':');
-        int id = stoi(v[0]);
-        string name = v[1];
-        station[name] = Station(name);
-        d[name] = 1e9;
-    }
-
 
     // 역 간 정보 정리
     ifstream in("subway_20224527.txt");
     string line;
-
+    string name1;
+    string name2;
     while (getline(in, line)) {// 역:역:시간:거리
         vector<string> v = split(line, ':');
-        string name1 = v[0];
-        string name2 = v[1];
+        name1 = v[0];
+        name2 = v[1];
+        station[name1] = Station(name1);
+        station[name2] = Station(name2);
         double time = stod(v[2]);
         double distance = stod(v[3]);
 
         station[name1].addNeighbor(station[name2], distance, time);
         station[name2].addNeighbor(station[name1], distance, time);
-
+        d[name1] = 1e9;
     }
+    d[name2] = 1e9;
     shortTime(start, d, station);
     cout << "걸린 시간 : " << d[end];
 }
